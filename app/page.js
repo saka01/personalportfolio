e863c88d0fffa6baba4d33779f910fddcc41f0f9
Saka <sakamoshood@ymail.com>
2023-06-03 21:16:57 +0000

    added sum footer

--- app/page.js
+++ app/page.js
@@ -9,23 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        
       </div>
       <div class="container d-flex h-100 align-items-center">
         <div class="mx-auto text-center">
@@ -85,6 +69,15 @@ export default function Home() {
           </p>
         </a>
       </div>
+
+      <div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            With ❤️ By 1000K
+          </a>
+        </div>
     </main>
   )
 }
