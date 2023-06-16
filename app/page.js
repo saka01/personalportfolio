ba9db37ed539703f8a310d986a2bca1f770a3e77
Saka <sakamoshood@ymail.com>
2023-06-16 00:00:22 -0400

    added quick redirect

--- app/page.js
+++ app/page.js
@@ -1,19 +1,40 @@
 import Image from "next/image";
+import yourImageUrl from "../public/images/mainbg.png";
 import styles from "./page.module.css";
 import RandomGreeting from "@/components/helper";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
+
   return (
     <section>
-      <Navbar />
-      <div>
-        <RandomGreeting />
-        <h1>👋🏾 Moshood Saka</h1>
-        <h2> Build + Secure</h2>
+      <div style={{ textAlign: "center", padding: "40px 0" }}>
+        <p style={{ fontSize: "30px" }}>👋🏾</p>
+        <p style={{ fontSize: "24px" }}>Hi, I'm Moshood!</p>
+        <br></br>
+        <h2 style={{ fontSize: "20px", color: "#888" }}>
+          I'm in the process of building a new portfolio. In the meantime, you
+          can view my previous portfolio
+        </h2>
+        <a
+          href="http://www.moshoodsaka.appspot.com"
+          style={lol}
+        >
+          Here
+        </a>
       </div>
       <Footer />
     </section>
   );
 }
+
+const lol = {
+    display: "inline-block",
+    padding: "10px 20px",
+    fontSize: "20px",
+    color: "white",
+    backgroundColor: "grey",
+    textDecoration: "none",
+    borderRadius: "25px",
+  };
--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,22 @@
-export default function Footer() {
-    return <footer>With ❤️ By 1000K</footer>;
-}
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer style={footerStyle}>
+      <p>1000K.</p>
+    </footer>
+  );
+};
+
+const footerStyle = {
+  position: "absolute",
+  bottom: 0,
+  width: "100%",
+  height: "2.5rem" /* Footer height */,
+  //   background: "#000000",
+  padding: "10px",
+  textAlign: "center",
+};
+
+
+export default Footer;
