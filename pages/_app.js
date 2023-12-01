85ab4b2b354c50f4fe3d3ee93a45b5431b76f94d
Saka <sakamoshood@ymail.com>
2023-12-01 12:33:34 -0500

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import "styles/globals.css";
 import RootLayout from "./layout";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 
 function MyApp({ Component, pageProps }) {
   const getLayout =
     Component.getLayout || ((page) => <RootLayout>{page}</RootLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
