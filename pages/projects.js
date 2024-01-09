85f526f46832ca855d7c2ca257109deb0b59a650
Saka <sakamoshood@ymail.com>
2024-01-09 13:08:17 -0500

    remove projects

--- pages/projects.js
+++ pages/projects.js
@@ -5,22 +5,6 @@ import styles from "@/styles/ProjectPage.module.css"; // Import the CSS module
 const ProjectPage = () => {
   const projects = [
     {
-      pname: "Heart Of The Nile",
-      briefDesc: "A restaurant website",
-      techStack: ["JS", "React"],
-      pic: "/images/hotn.png",
-      tag: "c",
-      year: 2022,
-    },
-    {
-      pname: "Hyperdealz",
-      briefDesc: "An E-Commerce Electronics Store",
-      techStack: ["JS", "React"],
-      pic: "/images/hotn.png",
-      tag: "c",
-      year: 2022,
-    },
-    {
       pname: "Interview scheduler UI",
       briefDesc: "A Simple ReactJS Scheduler",
       techStack: ["JS", "React"],
@@ -31,14 +15,6 @@ const ProjectPage = () => {
     {
       pname: "BAT$ Finance",
       briefDesc: "Crypto Current Prices & History",
-      techStack: ["JS", "React"],
-      pic: "/images/hotn.png",
-      tag: "p",
-      year: 2022,
-    },
-    {
-      pname: "Pivot Integrated",
-      briefDesc: "Consulting Website",
       techStack: ["JS", "React"],
       pic: "/images/hotn.png",
       tag: "p",
